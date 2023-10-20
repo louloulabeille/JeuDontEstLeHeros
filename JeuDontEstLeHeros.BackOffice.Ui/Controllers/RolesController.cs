0b26a7d279afc3a4d03199ab8cf141a7e7b786ca
louloulabeille <louloulabeille@hotmail.com>
2023-10-20 17:42:32 +0200

    Modification - au niveau de la création d'un role pour ajout d'un novueau message d'erreur.

--- JeuDontEstLeHeros.BackOffice.Ui/Controllers/RolesController.cs
+++ JeuDontEstLeHeros.BackOffice.Ui/Controllers/RolesController.cs
@@ -13,7 +13,7 @@ namespace JeuDontEstLeHeros.BackOffice.Ui.Controllers
         private readonly ILogger<RolesController> _logger;
 
         [BindProperty]
-        public RolesDTO? Roles {  get; set; }
+        public RolesDTO? Role {  get; set; } = new RolesDTO();
 
         public RolesController(RoleManager<IdentityRole> roleManager, ILogger<RolesController> logger)
         {
@@ -31,7 +31,7 @@ namespace JeuDontEstLeHeros.BackOffice.Ui.Controllers
             var result = _roleManager.Roles.Select(x => new RolesDTO()
             {
                 Id = x.Id,
-                Name = x.Name,
+                Name = x.Name?? string.Empty,
                 NormalizedName = x.NormalizedName,
                 ConcurrencyStamp = x.ConcurrencyStamp,
             });
@@ -47,22 +47,46 @@ namespace JeuDontEstLeHeros.BackOffice.Ui.Controllers
         // GET: RolesController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(Role);
         }
 
         // POST: RolesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create([FromForm] RolesDTO roles)
         {
+            ActionResult result = this.View(roles);
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (this.ModelState.IsValid && !string.IsNullOrEmpty(roles.Name))
+                {
+                    if (!await _roleManager.RoleExistsAsync(roles.Name))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = roles.Name,
+                            NormalizedName = roles.NormalizedName,
+                            ConcurrencyStamp = roles.ConcurrencyStamp,
+                        });
+                        result = this.RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        this.ModelState.AddModelError("Name", "Role déjà existant. Veuillez changer le nom.");
+                        result = this.View(roles);
+                    }
+                }
+                else
+                {
+                    result = this.View(roles);
+                }
             }
             catch
             {
-                return View();
+                result = this.NotFound();
             }
+            return result;
         }
 
         // GET: RolesController/Edit/5
--- JeuDontEstLeHeros.Core.Application/DTO/RolesDTO.cs
+++ JeuDontEstLeHeros.Core.Application/DTO/RolesDTO.cs
@@ -10,6 +10,7 @@ namespace JeuDontEstLeHeros.Core.Application.DTO
     public class RolesDTO
     {
         public string Id {  get; set; } = string.Empty;
+        [Required(ErrorMessage ="Le nom du role est obligatoire. Attention il ne doit pas exister dans la base.")]
         public string? Name { get; set; }
         public string? NormalizedName { get; set; }
         public string? ConcurrencyStamp { get; set; }

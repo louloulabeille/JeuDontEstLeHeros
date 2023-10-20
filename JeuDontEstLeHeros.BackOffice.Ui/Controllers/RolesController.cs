cea5e0c9fa9ad64e4d4b0cc50f20db86b1f2cb64
louloulabeille <louloulabeille@hotmail.com>
2023-10-20 16:43:01 +0200

    Controller Roles au niveau du Back

---
+++ JeuDontEstLeHeros.BackOffice.Ui/Controllers/RolesController.cs
@@ -0,0 +1,110 @@
+﻿using JeuDontEstLeHeros.Core.Application.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JeuDontEstLeHeros.BackOffice.Ui.Controllers
+{
+    [Authorize(Roles ="Administrator")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RolesController> _logger;
+
+        [BindProperty]
+        public RolesDTO? Roles {  get; set; }
+
+        public RolesController(RoleManager<IdentityRole> roleManager, ILogger<RolesController> logger)
+        {
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        // GET: RolesController
+        /// <summary>
+        /// affiche la liste des roles
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var result = _roleManager.Roles.Select(x => new RolesDTO()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                NormalizedName = x.NormalizedName,
+                ConcurrencyStamp = x.ConcurrencyStamp,
+            });
+            return View(result);
+        }
+
+        // GET: RolesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RolesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RolesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RolesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RolesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
